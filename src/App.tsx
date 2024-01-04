1173da6c09ba22dcf00f9582d21121cf3c192549
Scott <scott.ty.ma@gmail.com>
2024-01-04 14:53:38 -0600

    tailwind set up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='w-screen h-screen bg-orange-300 flex flex-col justify-center items-center'>
+      <h1>Header</h1>
+    </section>
+  )
+}
+
+export default App
